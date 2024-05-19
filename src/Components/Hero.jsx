4bf6c740b3d7d0f372863b2e400d5c9ce7c0a38e
shreeniwash <68107824+shreeniwash@users.noreply.github.com>
2024-05-20 00:46:41 +0530

    Initial commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,21 +7,21 @@ const Hero = () => {
   <div className="max-w-7xl mx-auto p-4 md:p-8">
     <img src="hero.png" alt="hero" className="w-full" />
     <div className='flex flex-wrap justify-center py-20 gap-20 md:gap-12 '>
-      <div classNameName='flex flex-col justify-center gap-4  md:gap-12 sm:flex-col md:flex-row lg:flex-row min-w-full '>
-        <p classNameName=' font-custom1  text-[30px] sm:text-6xl  md:text-4xl  lg:text-5xl xl:text-7xl 2xl:w-8xl px-2 py-3 md:px-5 md:py-7  border-2 border-color2 rounded-lg text-color4 text-center'>Stay Ahead of the Game in</p>
-        <img src='img2.jpeg' classNameName='rounded-full w-full h-28 md:h-auto lg:h-auto xl:h-auto   md:w-1/4 lg:w-1/4 xl:w-1/4 2xl:w-1/4 '></img>
+      <div className='flex flex-col justify-center gap-4  md:gap-12 sm:flex-col md:flex-row lg:flex-row min-w-full '>
+        <p className=' font-custom1  text-[30px] sm:text-6xl  md:text-4xl  lg:text-5xl xl:text-7xl 2xl:w-8xl px-2 py-3 md:px-5 md:py-7  border-2 border-color2 rounded-lg text-color4 text-center'>Stay Ahead of the Game in</p>
+        <img src='img2.jpeg' className='rounded-full w-full h-28 md:h-auto lg:h-auto xl:h-auto   md:w-1/4 lg:w-1/4 xl:w-1/4 2xl:w-1/4 '></img>
       </div>
       
 
-      <div classNameName='flex flex-col justify-center gap-4  md:gap-12 sm:flex-col md:flex-row lg:flex-row min-w-full '>
-      <img src='img3.jpeg' classNameName='rounded-full  h-28 md:h-28 lg:h-28 xl:h-40  w-full md:w-[55%]  '></img>
-        <p classNameName=' font-custom1  text-[30px] sm:text-6xl   md:text-4xl lg:text-5xl xl:text-7xl 2xl:w-8xl px-2 py-3 md:px-5 md:py-7  border-2 border-color2 rounded-lg text-color4 text-center'>Digital World</p>
+      <div className='flex flex-col justify-center gap-4  md:gap-12 sm:flex-col md:flex-row lg:flex-row min-w-full '>
+      <img src='img3.jpeg' className='rounded-full  h-28 md:h-28 lg:h-28 xl:h-40  w-full md:w-[55%]  '></img>
+        <p className=' font-custom1  text-[30px] sm:text-6xl   md:text-4xl lg:text-5xl xl:text-7xl 2xl:w-8xl px-2 py-3 md:px-5 md:py-7  border-2 border-color2 rounded-lg text-color4 text-center'>Digital World</p>
        
       </div>
 
-      <div classNameName='flex flex-col justify-center gap-4  md:gap-12 sm:flex-col md:flex-row lg:flex-row min-w-full '>
-      <p classNameName=' font-custom1  text-[30px] sm:text-6xl   md:text-4xl lg:text-5xl xl:text-7xl 2xl:w-8xl px-2 py-3 md:px-5 md:py-7  border-2 border-color2 rounded-lg text-color4 text-center'>Cybersecurity</p>
-      <img src='img4.jpeg' classNameName='rounded-full  h-28 md:h-28 lg:h-28 xl:h-40  w-full md:w-2/3   '></img>
+      <div className='flex flex-col justify-center gap-4  md:gap-12 sm:flex-col md:flex-row lg:flex-row min-w-full '>
+      <p className=' font-custom1  text-[30px] sm:text-6xl   md:text-4xl lg:text-5xl xl:text-7xl 2xl:w-8xl px-2 py-3 md:px-5 md:py-7  border-2 border-color2 rounded-lg text-color4 text-center'>Cybersecurity</p>
+      <img src='img4.jpeg' className='rounded-full  h-28 md:h-28 lg:h-28 xl:h-40  w-full md:w-2/3   '></img>
        
        
       </div>
